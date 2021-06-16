e6f35ea2e23bd643e968a3449cc0e441e394f01a
Sumit Kumar <sumit.kumar@galepartners.com>
2021-06-16 22:34:48 +0530

    Implemented Search API

--- server/apps/video_search/views.py
+++ server/apps/video_search/views.py
@@ -5,14 +5,22 @@ from rest_framework import generics
 # from rest_framework import filters
 from rest_framework.views import APIView
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .serializers import VideoSerializer
 from .models import Videos
 
+
+class VideoDataApiView(generics.ListAPIView):
+
+    serializer_class = VideoSerializer
+    pagination_class = LimitOffsetPagination
+    queryset = Videos.objects.all().order_by('-published_at')
 
 class VideoSearchApiView(generics.ListAPIView):
 
     serializer_class = VideoSerializer
     pagination_class = LimitOffsetPagination
     queryset = Videos.objects.all().order_by('-published_at')
-
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ['title']
