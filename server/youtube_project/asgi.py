eb94f789dff87b5c0888c378ced0cff41e505bf6
Sumit Kumar <sumit.kumar@galepartners.com>
2021-06-16 09:16:27 +0530

    Project Setup changes, site modules error fixes

--- server/youtube_project/asgi.py
+++ server/youtube_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI youtube_search for backend project.
+ASGI youtube_project for backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_search.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_project.settings')
 
 application = get_asgi_application()
--- server/youtube_project/urls.py
+++ server/youtube_project/urls.py
@@ -1,4 +1,4 @@
-"""youtube_search URL Configuration
+"""youtube_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
