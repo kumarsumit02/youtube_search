81079dd77d16c9fb0c65897c24f5a15636f248f2
Sumit Kumar <sumit.kumar@galepartners.com>
2021-06-17 10:19:35 +0530

    Changed indexing to published date

--- server/apps/video_search/models.py
+++ server/apps/video_search/models.py
@@ -3,9 +3,9 @@ import pgcrypto
 
 
 class Videos(models.Model):
-    title = models.CharField(max_length=500, blank=False, db_index=True)
-    description = models.TextField(blank=True, db_index=True)
-    published_at = models.DateTimeField(blank=False)
+    title = models.CharField(max_length=500, blank=False)
+    description = models.TextField(blank=True)
+    published_at = models.DateTimeField(blank=False, db_index=True)
     thumbnail_url = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
